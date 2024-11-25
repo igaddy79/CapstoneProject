fea00143cbf7c791de0e9e3fb646e57f7afea13d
igaddy79 <igaddy79@yahoo.com>
2024-11-24 21:18:00 -0500

    comments table SQL Queries work with adding comments to reviews for specific movies.

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const connectToDatabase = async () => {
 };
 
 const init = async () => {
-  await client.connect();
+  //await client.connect();
   console.log("Connected to database");
   await createTables();
   console.log("Tables created");
@@ -35,7 +35,7 @@ const init = async () => {
     createMovie({ name: 'Hamilton', description: 'The real life of one of Americas foremost founding fathers and first Secretary of the Treasury, Alexander Hamilton. Captured live on Broadway from the Richard Rodgers Theater with the original Broadway cast.', image: 'https://i5.walmartimages.com/asr/149d1fd0-2254-421f-89d8-fe8d0f879b2d.45ce4ae056c8c0b3b1fce677f437a252.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF', genre: 'History'})
   ])
 
-  app.listen(port, () => console.log(`listening on port ${port}`));
+  //app.listen(port, () => console.log(`listening on port ${port}`));
 };
 
 init();
